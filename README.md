c102c15843da8ba29c04912f3bd4cd1a51508c03
Stezii <stefan.zimmer_shb@t-online.de>
2017-05-05 10:40:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# tilestrata-postgismvt
+[![NPM version](http://img.shields.io/npm/v/tilestrata-postgismvt.svg?style=flat)](https://www.npmjs.org/package/tilestrata-postgismvt)
+
+A [TileStrata](https://github.com/naturalatlas/tilestrata) plugin for retrieving [Mapbox Vector Tiles](https://github.com/mapbox/vector-tile-spec) from a PostGIS database.
+
+```sh
+$ npm install tilestrata-postgismvt --save
+```
+
+### Sample Usage
+
+Serve all map features.
+![default](img/default.png?raw=true "default")
+
+```js
+var postgismvt = require('tilestrata-postgismvt');
+
+server.layer('mylayer').route('tile.mvt')
+  .use(postgismvt({
+    lyr: {
+      table: 'schema.table',
+      geometry: 'geom',
+      type: 'circle',
+      srid: 4326,
+      minZoom: 3,
+      maxZoom: 19,
+      buffer: 10
+      fields: 'name gid',
+      resolution: 256,
+    },
+    pgConfig: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'mypassword',
+      database: 'postgres',
+      port: '5432'
+    }}))
+  );
+```
+
+Cluster map features sharing a coordinate in the tile coordinate space.
+Attributes will be dropped. Each feature contains an attribute *count* that can be used e. g. for a density map.
+![cluster](img/cluster.png?raw=true "cluster")
+
+```js
+server.layer('mylayer').route('tile.mvt')
+  .use(postgismvt({
+    lyr: {
+      ...
+      mode: 'cluster'
+    },
+    pgConfig: {
+      ...
+    }}))
+  );
+```
+
+Cluster map features sharing a coordinate in the tile coordinate space.
+Attributes will be concatenated. Each feature contains an attribute *count* that can be used e. g. for a density map.
+![cluster_fields](img/cluster_fields.png?raw=true "cluster_fields")
+
+```js
+server.layer('mylayer').route('tile.mvt')
+  .use(postgismvt({
+    lyr: {
+      ...
+      mode: 'cluster_fields'
+    },
+    pgConfig: {
+      ...
+    }}))
+  );
+```
+
+*resolution* and *mode* support functions. The following configuration serves attributes in higher zoom levels only and increases the geographic accuracy.
+
+```js
+server.layer('mylayer').route('tile.mvt')
+  .use(postgismvt({
+    lyr: {
+      resolution: function(server, req) {
+        if (req.z > 12) return 512;
+        return 256;
+      },
+      mode: function(server, req) {
+        if (req.z > 15) return null;
+        if (req.z > 13) return 'cluster_fields';
+        return 'cluster';
+      }
+    },
+    pgConfig: {
+      ...
+    }}))
+  );
+```
+
+## Requirements
+
+- PostGIS 2.4.0
+- [TileBBox.sql](https://github.com/mapbox/postgis-vt-util/blob/master/src/TileBBox.sql)
+
+## License
+
+Copyright &copy; 2017 [Stefan Zimmer](https://github.com/Stezii)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ index.js
@@ -0,0 +1,122 @@
+var pg = require('pg');
+const SQL = require('sql-template-strings');
+
+module.exports = function(options) {
+
+	var pgPool;
+	var lyr;
+
+	/**
+	 * Initializes the layer config and the PostgreSQL datasource.
+	 *
+	 * @param {TileServer} server
+	 * @param {function} callback(err, fn)
+	 * @return {void}
+	 */
+	function initialize(server, callback) {
+		lyr = options.lyr;
+		pgPool = new pg.Pool(options.pgConfig);
+		pgPool.on('error', function (err, client) {
+		  console.error(err.message, err.stack);
+			var err = new Error('idle client error');
+			err.statusCode = 500;
+			callback(err);
+		});
+		callback(null);
+	}
+
+	/**
+	 * Creates a tile and returns the result as a Mapbox Vector Tile,
+	 * plus the headers that should accompany it.
+	 *
+	 * @param {TileServer} server
+	 * @param {TileRequest} tile
+	 * @param {function} callback(err, buffer, headers)
+	 * @return {void}
+	 */
+	function serveMVT(server, tile, callback) {
+		if (tile.z < lyr.minZoom || tile.z > lyr.maxZoom) {
+			err = new Error('Request out of zoom level bounds');
+			err.statusCode = 204;
+			return callback(err);
+		}
+
+		var fields = lyr.fields ? ', ' + lyr.fields.split(' ') : '';
+		var clip_geom = (lyr.buffer > 0) ? true : false;
+		var resolution = (typeof lyr.resolution === 'function') ? lyr.resolution(server, tile) : lyr.resolution;
+		var mode = (typeof lyr.mode === 'function') ? lyr.mode(server, tile) : lyr.mode;
+
+		var query;
+		switch (mode) {
+
+			case "cluster":
+				var agg_q_name = 'mvt_geo';
+				query = `
+					SELECT ST_AsMVT('${tile.layer}', ${resolution}, 'geom', q) AS mvt FROM (
+						WITH ${agg_q_name} AS (
+							SELECT 1 cnt, ST_AsMVTGeom(ST_Transform(${lyr.table}.${lyr.geometry}, 3857), TileBBox(${tile.z}, ${tile.x}, ${tile.y}, 3857), ${resolution}, ${lyr.buffer}, ${clip_geom}) geom
+							FROM ${lyr.table}
+							WHERE ST_Intersects(TileBBox(${tile.z}, ${tile.x}, ${tile.y}, ${lyr.srid}), ${lyr.table}.${lyr.geometry})
+						)
+						SELECT COUNT(${agg_q_name}.cnt), ${agg_q_name}.geom
+						FROM ${agg_q_name}
+						GROUP BY ${agg_q_name}.geom
+					) AS q
+				`;
+				break;
+
+			case "cluster_fields":
+				var agg_q_name = 'mvt_geo';
+				var fieldsAgg = '';
+				if (lyr.fields) {
+					lyr.fields.split(' ').forEach(function(field) {
+						fieldsAgg += ', string_agg('+agg_q_name+'.' + field + '::text, \',\') AS ' + field;
+					});
+				}
+				query = `
+				SELECT ST_AsMVT('${tile.layer}', ${resolution}, 'geom', q) AS mvt FROM (
+					WITH ${agg_q_name} AS (
+						SELECT 1 cnt, ST_AsMVTGeom(ST_Transform(${lyr.table}.${lyr.geometry}, 3857), TileBBox(${tile.z}, ${tile.x}, ${tile.y}, 3857), ${resolution}, ${lyr.buffer}, ${clip_geom}) geom ${fields}
+						FROM ${lyr.table}
+						WHERE ST_Intersects(TileBBox(${tile.z}, ${tile.x}, ${tile.y}, ${lyr.srid}), ${lyr.table}.${lyr.geometry})
+					)
+					SELECT COUNT(${agg_q_name}.cnt), ${agg_q_name}.geom ${fieldsAgg}
+					FROM ${agg_q_name}
+					GROUP BY ${agg_q_name}.geom
+				) AS q
+				`;
+				break;
+
+				default:
+					query = `
+						SELECT ST_AsMVT('${tile.layer}', ${resolution}, 'geom', q) AS mvt FROM (
+							SELECT ST_AsMVTGeom(ST_Transform(${lyr.table}.${lyr.geometry}, 3857), TileBBox(${tile.z}, ${tile.x}, ${tile.y}, 3857), ${resolution}, ${lyr.buffer}, ${clip_geom}) geom ${fields}
+							FROM ${lyr.table}
+							WHERE ST_Intersects(TileBBox(${tile.z}, ${tile.x}, ${tile.y}, ${lyr.srid}), ${lyr.table}.${lyr.geometry})
+						) AS q
+					`;
+					break;
+		}
+
+		pgPool.query(query, function(err, result) {
+			if (err) {
+				console.log(query, err.message, err.stack)
+				var err = new Error('An error occurred');
+				err.statusCode = 500;
+				return callback(err);
+			}
+			if (!result.rows[0].mvt) {
+				err = new Error('No data');
+				err.statusCode = 204;
+				return callback(err);
+			}
+			callback(null, result.rows[0].mvt, {'Content-Type': 'application/x-protobuf'});
+		});
+	}
+
+	return {
+		name: 'postgismvt',
+    init: initialize,
+		serve: serveMVT
+	};
+};
